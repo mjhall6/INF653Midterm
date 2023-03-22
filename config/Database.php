1eac062294376155d5e0718c7e7fc783c8d5e6d6
Megan <megjordan1021@gmail.com>
2023-03-22 15:32:01 -0400

    fixing typos in Database Class

--- config/Database.php
+++ config/Database.php
@@ -26,10 +26,8 @@
         // Connection already exists
         return $this->conn;        
       } else {
-        
-        $dsn = "pgsql:host={this->host};port={this->port};dbname={$this->dbname};";
+        $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname};";
       }
-
       try {
         $this->conn = new PDO($dsn, $this->username, $this->password);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
