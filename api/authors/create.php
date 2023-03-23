96cd4ec5d9faef58f0253ece1d15f3d4b7577044
Megan <megjordan1021@gmail.com>
2023-03-22 23:37:14 -0400

    updated create.phpfor AUthors

--- api/authors/create.php
+++ api/authors/create.php
@@ -20,17 +20,23 @@
     // Get raw posted data
     $data = json_decode(file_get_contents("php://input"));
 
+    if(!isset($data->author)) {
+        echo json_encode(
+            array('message' => 'Missing Required Parameters')
+        );
+        exit();
+    }
+
     $author->author = $data->author;
-    $author->id = $data->id;
 
     // Create Author
     if($author->create()) {
         echo json_encode(
-            array('message' => 'Author Created')
+            array('id'=>$author->id, 'author'=>$author->author)
         );
     } else {
         echo json_encode(
-            array('message' => 'Author Not Created')
+            array('message' => 'No Authors Found')
         );
     }
 ?>
